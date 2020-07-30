e7318fb587d083be5e6980f613a89d6be2e24359
gerrardxcc <chengcheng.xiong@my.jcu.edu.au>
2020-07-30 21:46:36 +1000

    Exercise 2.1 has been completed successfully and code executed without any bugs

--- src/PrintInitials.java
+++ src/PrintInitials.java
@@ -2,6 +2,17 @@
 public class PrintInitials {
 
     public static void main(String[] args) {
+        System.out.println();
+        System.out.println("           ******           *************        **********");
+        System.out.println("           **    **                **            **");
+        System.out.println("           **     **               **            **");
+        System.out.println("           **      **              **            **");
+        System.out.println("           **      **              **            ********");
+        System.out.println("           **      **       **     **            **");
+        System.out.println("           **     **         **    **            **");
+        System.out.println("           **    **           **  **             **");
+        System.out.println("           *****               ****              **********");
+        System.out.println();
         // TODO - add code here to complete Exercise 2.1
     }  // end main()
 
