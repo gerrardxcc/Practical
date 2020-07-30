15ce1b7b5eb9dafd85e7c91de860a5006bf3a524
gerrardxcc <chengcheng.xiong@my.jcu.edu.au>
2020-07-31 02:32:54 +1000

    Exercise 2.4 has been completed successfully and code executed without any bugs.

--- src/CountChange.java
+++ src/CountChange.java
@@ -1,8 +1,49 @@
-import textio.TextIO;
+import java.util.Scanner;
 
 public class CountChange {
 
     public static void main(String[] args) {
+        int fiveCents;
+        int tenCents;
+        int twentyCents;
+        int fiftyCents;
+        int oneDollar;
+        int twoDollars;
+        double dollars;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please enter the number of 5c: ");
+        fiveCents = input.nextInt();
+        input.nextLine();
+
+        System.out.println("Please enter the number of 10c: ");
+        tenCents = input.nextInt();
+        input.nextLine();
+
+        System.out.println("Please enter the number of 20c: ");
+        twentyCents = input.nextInt();
+        input.nextLine();
+
+        System.out.println("Please enter the number of 50c: ");
+        fiftyCents = input.nextInt();
+        input.nextLine();
+
+        System.out.println("Please enter the number of $1: ");
+        oneDollar = input.nextInt();
+        input.nextLine();
+
+        System.out.println("Please enter the number of $2: ");
+        twoDollars = input.nextInt();
+        input.nextLine();
+
+        dollars = (0.05 * fiveCents) + (0.10 * tenCents) + (0.20 * twentyCents)
+                + (0.50 * fiftyCents) + (1.00 * oneDollar) + (2.00 * twoDollars);
+        System.out.println();
+        System.out.print("The total in dollars is $ ");
+        System.out.printf("%1.2f", dollars);
+        System.out.println();
+
         // TODO - add code here to complete Exercise 2.4
     }  // end main()
 
