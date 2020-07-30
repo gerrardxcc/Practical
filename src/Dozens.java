a9be6e604df605ef96e0eda442e38e9ad2ff6604
gerrardxcc <chengcheng.xiong@my.jcu.edu.au>
2020-07-31 02:52:29 +1000

    Exercise 2.5 has been completed successfully and code executed without any bugs.

--- src/Dozens.java
+++ src/Dozens.java
@@ -1,8 +1,29 @@
 // TODO - add the appropriate import here
 
+import textio.TextIO;
+
 public class Dozens {
 
     public static void main(String[] args) {
+        int eggs;
+        int dozenEggs;
+        int leftOver;
+
+        System.out.println("How many eggs do you have? ");
+        eggs = TextIO.getInt();
+
+        dozenEggs = eggs / 12;
+        leftOver = eggs % 12;
+
+        System.out.print("Your number of eggs is ");
+        System.out.print(dozenEggs);
+        System.out.print(" dozen and ");
+        System.out.print(leftOver);
+        System.out.println();
+
+
+
+
         // TODO - add code here to complete Exercise 2.5
     }  // end main()
 
