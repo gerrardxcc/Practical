bc4514da65d130b76bb1e3e739329723aa91f380
gerrardxcc <chengcheng.xiong@my.jcu.edu.au>
2020-07-30 21:47:58 +1000

    Exercise 2.2 has been completed successfully and code executed without any bugs.

--- src/RollTheDice.java
+++ src/RollTheDice.java
@@ -2,6 +2,18 @@
 public class RollTheDice {
 
     public static void main(String[] args) {
+        int die3;
+        int die5;
+        int roll;
+
+        die3 =(int)(Math.random()*6)+1;
+        die5 =(int)(Math.random()*6)+1;
+        roll = die3+die5;
+
+        System.out.println("The first die comes up " + die3);
+        System.out.println("The first die comes up " + die5);
+        System.out.println("Your total roll is "+ roll);
+
         // TODO - add code here to complete Exercise 2.2
     }  // end main()
 
