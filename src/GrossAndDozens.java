debf9768d5ea323af34d700ccd269d6839e326d7
gerrardxcc <chengcheng.xiong@my.jcu.edu.au>
2020-07-31 02:55:27 +1000

    Exercise 2.5 has been completed successfully and code executed without any bugs.

--- src/GrossAndDozens.java
+++ src/GrossAndDozens.java
@@ -1,8 +1,32 @@
 // TODO - add the appropriate import here
 
+import textio.TextIO;
+
 public class GrossAndDozens {
 
     public static void main(String[] args) {
+        int eggs;
+        int gross;
+        int aboveGross;
+        int dozenEggs;
+        int leftOver;
+
+        System.out.println("How many eggs do you have? ");
+        eggs = TextIO.getInt();
+
+        gross = eggs / 144;
+        aboveGross = eggs % 144;
+
+        dozenEggs = aboveGross/ 12;
+        leftOver = aboveGross % 12;
+
+        System.out.print("Your number of eggs is ");
+        System.out.print(gross);
+        System.out.print(" gross, ");
+        System.out.print(dozenEggs);
+        System.out.print(" dozen, and ");
+        System.out.print(leftOver);
+        System.out.println();
         // TODO - add code here to complete Exercise 2.5
     }  // end main()
 
