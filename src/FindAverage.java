e62a92a48214f7b8840ca9088b2d9c2a40b33a49
gerrardxcc <chengcheng.xiong@my.jcu.edu.au>
2020-07-31 17:12:58 +1000

    Exercise 2.7 has been completed successfully but code cannot executed and there is showing the red colour on the project files.

--- src/FindAverage.java
+++ src/FindAverage.java
@@ -1,9 +1,26 @@
 // TODO - add the appropriate import here
 
+import textio.TextIO;
+
 public class FindAverage {
 
     public static void main(String[] args) {
         // TODO - add code here to complete Exercise 2.7
+        String name;
+        int exam1,exam2,exam3;
+        double average;
+
+        TextIO.readFile("testdata.txt");
+
+        name = TextIO.getln();
+        exam1 = TextIO.getInt();
+        exam2 = TextIO.getInt();
+        exam3 = TextIO.getInt();
+
+        average = (exam1 + exam2 + exam3) / 3.0;
+
+        System.out.printf("The average grade for %s was %1.1f", name, average);
+        System.out.println();
     }
 
 }
