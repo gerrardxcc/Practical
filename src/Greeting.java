c0d6d7818a6e658027e39314f980af91de435341
gerrardxcc <chengcheng.xiong@my.jcu.edu.au>
2020-07-31 19:20:00 +1000

    Exercise 2.3 has been updated with the correct colon at the end of println() and executed successfully.

--- src/Greeting.java
+++ src/Greeting.java
@@ -5,13 +5,14 @@ public class Greeting {
     public static void main(String[] args) {
         String userName;
         String upperCaseName;
-        System.out.println("Please enter your name ");
+        System.out.println("Please enter your name: ");
         Scanner input = new Scanner(System.in);
         userName = input.next();
 
         upperCaseName = userName.toUpperCase();
 
         System.out.println("Hello, "+ upperCaseName + ", nice to meet you!");
+
 
         // TODO - add code here to complete Exercise 2.3
     }  // end main()
